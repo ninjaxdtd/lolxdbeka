374551615cc25e1abf147b048a314fd6bcfe8674
Kamil Mendecki <kamil.mendecki@wp.pl>
2023-09-15 13:18:02 +0200

    dodano kwerende

--- login.php
+++ login.php
@@ -10,7 +10,9 @@ DEFINE('user','root');
 DEFINE('password',' ');
 
 $conn =mysqli_connect(host,user,password);
-$db = mysqli_select_db($conn)
+$db = mysqli_select_db($conn, 'baza_pracownicy');
+
+$kwerenda ="Select";
 
 
 ?>
