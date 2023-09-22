7bd3abdd836d77200f08a07300597fee4339f7d1
Kamil Mendecki <kamil.mendecki@wp.pl>
2023-09-22 12:28:54 +0200

    dodanie stylu

--- login.php
+++ login.php
@@ -7,12 +7,7 @@ DEFINE('host','localhost');
 
 DEFINE('user','root');
 
-DEFINE('password',' ');
-
-$conn =mysqli_connect(host,user,password);
-$db = mysqli_select_db($conn, 'baza_pracownicy');
-
-$kwerenda ="Select";
+DEFINE('pass',' ');
 
 
 ?>
